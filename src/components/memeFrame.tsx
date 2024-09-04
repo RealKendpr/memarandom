b3d4edda44902cf13cfdc3346608436957248c54
kendpr <real.kendpr@gmail.com>
2024-09-04 08:28:51 +0800

    Disable react button in first page load since theres is no meme to react to

--- src/components/memeFrame.tsx
+++ src/components/memeFrame.tsx
@@ -6,14 +6,16 @@ export default function MemeFrame() {
   return (
     <div className="img grid place items-center min-h-dvh">
       {state.firstLoad ? (
-        <div>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut iure
-          assumenda sint, cum facere adipisci explicabo neque ut totam quas id
-          nisi aliquid repellat unde voluptas dolorem magnam commodi nostrum.
+        <div className="text-zinc-300">
+          <strong>Lorem ipsum dolor sit amet.</strong> <br />
+          <p className="mt-4">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut iure
+            assumenda sint, cum facere adipisci explicabo neque ut totam quas id
+            nisi aliquid repellat unde voluptas dolorem magnam commodi nostrum.
+          </p>
         </div>
       ) : (
         <img className="select-none" src="./meme.png" alt="a meme" />
-        // <div className="text-white">{state.count}</div>
       )}
     </div>
   );
