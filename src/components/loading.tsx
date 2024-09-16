654bb6c31ef0bd2cbd550d2a16680cb08661e7ca
kendpr <119304897+RealKendpr@users.noreply.github.com>
2024-09-16 13:36:05 +0800

    update loading image

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -7,12 +7,12 @@ export default function Loading() {
     state.loading && (
       <div className="text-zinc-200 absolute w-full min-h-dvh grid place-content-center">
         <img
-          src="/meme-face.png"
+          src="/loading.gif"
           alt="Bouncing troll face"
-          className="animate-bounce size-16 select-none"
+          className="select-none mb-4"
           aria-hidden="true"
         />
-        <p className="text-lg font-medium">Picking a meme for you...</p>
+        <p className="text-xl font-medium">Picking a meme for you...</p>
         {state.firstLoad && (
           <p>
             <i>First time loading might take a while.</i>
